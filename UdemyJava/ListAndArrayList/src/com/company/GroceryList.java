b289253b7e59be652192c680a16cd559396b6379
reycika <renata.atwork@gmail.com>
2017-06-07 16:03:13 +0200

    Add files via upload

---
+++ UdemyJava/ListAndArrayList/src/com/company/GroceryList.java
@@ -0,0 +1,73 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if (position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    private void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if (position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+
+    private void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        System.out.println("Grocery item " + theItem + " has been removed.");
+
+        // Milk
+        // Cheese
+        // Bread
+    }
+
+    private int findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+
+        return groceryList.indexOf(searchItem);
+
+//        int position = groceryList.indexOf(searchItem);
+//        if (position >= 0) {
+//            return groceryList.get(position);
+//        }
+//        return null;
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if (position >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
